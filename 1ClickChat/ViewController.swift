9114a7a8dd8faa6c270fada7ce3965e24b2699b5
小川秀哉 <shuya464924@gmail.com>
2017-10-25 14:45:14 +0900

    Initial Commit

---
+++ 1ClickChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  1ClickChat
+//
+//  Created by 小川秀哉 on 2017/10/25.
+//  Copyright © 2017年 Digital Circus Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

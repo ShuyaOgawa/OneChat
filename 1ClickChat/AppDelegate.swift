071830dd0278b0903bc4587b354fc15906fed22b
小川秀哉 <shuya464924@gmail.com>
2017-11-20 00:14:17 +0900

    get id

--- 1ClickChat/AppDelegate.swift
+++ 1ClickChat/AppDelegate.swift
@@ -20,4 +20,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             FirebaseApp.configure()
             return true
     }
+    
+    
 }
--- 1ClickChat/StartViewController.swift
+++ 1ClickChat/StartViewController.swift
@@ -14,6 +14,10 @@ class StartViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let ref = Database.database().reference()
+        ref.child("user/all_member/0").setValue("must_not_delete")
+        ref.child("user/waiting_member/0").setValue("must_not_delete")
+
 
         // Do any additional setup after loading the view.
     }
@@ -27,29 +31,52 @@ class StartViewController: UIViewController {
     @IBAction func StartButton(_ sender: UIButton) {
         let ref = Database.database().reference()
         
-       
-        ref.child("messages").child("-KzHdhFa1hwJaTRJFGPq").observeSingleEvent(of: .value, with: { (snapshot) in
+        
+        
+   /*
+        ref.child("messages").child("-KzIaGarqKKCP7wkHf-E").observeSingleEvent(of: .value, with: { (snapshot) in
             // Get user value
             let value = snapshot.value as? NSDictionary
+            print(value)
             let username = value?["text"] as? String ?? ""
             print("aaaaaaaaaaaaaaa")
             print(username)
         }) { (error) in
             print(error.localizedDescription)
         }
+ */
+ 
         
-        ref.child("all_member").child("id").observeSingleEvent(of: .value, with: { (snapshot) in
+        ref.child("user").observeSingleEvent(of: .value, with: { (snapshot) in
             // Get user value
             let value = snapshot.value as? NSDictionary
-            let username = value?["id"] as? String ?? ""
-            print("aaaaaaaaaaaaaaa")
-            print(username)
+            print(value)
+            let all_member_array = value?["all_member"] as AnyObject?
+            print("ccccccccccccccccccccccc")
+            print(all_member_array?.count)
+            let my_id = all_member_array?.count
+            ref.child("user/all_member/\(my_id!)").setValue(my_id!)
+            ref.child("user/waiting_member/\(my_id!)").setValue(my_id!)
         }) { (error) in
             print(error.localizedDescription)
         }
        
+//        ref.child("user/01").setValue("2")
+//        ref.child("user/02").setValue("2")
+//        ref.child("user/02").setValue("3")
+//        ref.child("user/03")
+ /*       for i in 0..<1{
+            ref.child("user/all_member/\(i)").setValue("must_not_delete")
+        }
+*/
+/*        let data = [0]
+        ref.child("user/all_member").setValue(data)
+        
+ */
         
     }
+    
+  
     /*
     // MARK: - Navigation
 

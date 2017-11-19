6a6ea5a90dfbd5a8161db8b6de686c73442c93e7
小川秀哉 <shuya464924@gmail.com>
2017-11-20 00:53:04 +0900

    merge

---
+++ 1ClickChat/StartViewController.swift
@@ -0,0 +1,93 @@
+//
+//  StartViewController.swift
+//  1ClickChat
+//
+//  Created by 小川秀哉 on 2017/11/19.
+//  Copyright © 2017年 Digital Circus Inc. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseDatabase
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let ref = Database.database().reference()
+        ref.child("user/all_member/0").setValue("must_not_delete")
+        ref.child("user/waiting_member/0").setValue("must_not_delete")
+
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func StartButton(_ sender: UIButton) {
+        let ref = Database.database().reference()
+        
+        
+        
+   /*
+        ref.child("messages").child("-KzIaGarqKKCP7wkHf-E").observeSingleEvent(of: .value, with: { (snapshot) in
+            // Get user value
+            let value = snapshot.value as? NSDictionary
+            print(value)
+            let username = value?["text"] as? String ?? ""
+            print("aaaaaaaaaaaaaaa")
+            print(username)
+        }) { (error) in
+            print(error.localizedDescription)
+        }
+ */
+ 
+        
+        ref.child("user").observeSingleEvent(of: .value, with: { (snapshot) in
+            // Get user value
+            let value = snapshot.value as? NSDictionary
+            print(value)
+            let all_member_array = value?["all_member"] as AnyObject?
+            print("ccccccccccccccccccccccc")
+            print(all_member_array?.count)
+            let my_id = all_member_array?.count
+            ref.child("user/all_member/\(my_id!)").setValue(my_id!)
+            ref.child("user/waiting_member/\(my_id!)").setValue(my_id!)
+        }) { (error) in
+            print(error.localizedDescription)
+        }
+       
+//        ref.child("user/01").setValue("2")
+//        ref.child("user/02").setValue("2")
+//        ref.child("user/02").setValue("3")
+//        ref.child("user/03")
+ /*       for i in 0..<1{
+            ref.child("user/all_member/\(i)").setValue("must_not_delete")
+        }
+*/
+/*        let data = [0]
+        ref.child("user/all_member").setValue(data)
+        
+ */
+        
+    }
+    
+  
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+    
+
+}

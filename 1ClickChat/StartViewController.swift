727b0d189e2b3d98e189f1464b3669b0a439d517
小川秀哉 <shuya464924@gmail.com>
2017-11-19 14:54:29 +0900

    StartViewController

---
+++ 1ClickChat/StartViewController.swift
@@ -0,0 +1,35 @@
+//
+//  StartViewController.swift
+//  1ClickChat
+//
+//  Created by 小川秀哉 on 2017/11/19.
+//  Copyright © 2017年 Digital Circus Inc. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

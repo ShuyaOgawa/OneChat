5171f13ec1c9aec46b97ddbdea0148ef855598c6
小川秀哉 <shuya464924@gmail.com>
2017-11-26 14:04:40 +0900

    activator

--- 1ClickChat/StartViewController.swift
+++ 1ClickChat/StartViewController.swift
@@ -13,6 +13,7 @@ import FirebaseDatabase
 class StartViewController: UIViewController {
     
     var appDelegate:AppDelegate = UIApplication.shared.delegate as! AppDelegate
+    var ActivityIndicator: UIActivityIndicatorView!
     
     
     override func viewDidLoad() {
@@ -49,6 +50,10 @@ class StartViewController: UIViewController {
             
             //①自分が1人目のパターン
             if waiting_member_array?.count == 1{
+                
+                //ローディング
+                self.loading()
+               
                 print("pattern 1")
                 ref.child("user/waiting_member/\(my_id!)").setValue(my_id!)
                 self.appDelegate.my_id = my_id! as AnyObject
@@ -61,6 +66,11 @@ class StartViewController: UIViewController {
                         let all_member_array = user_array!["all_member"] as AnyObject?
                         let all_member_count = all_member_array?.count as Int?
                         self.appDelegate.your_id = all_member_count! - 1 as AnyObject
+                        
+                        //ロード終了
+                        self.ActivityIndicator.stopAnimating()
+                        
+                        
                         self.performSegue(withIdentifier: "SegueId", sender: self)
                         
                         //chat_roomを作る
@@ -103,10 +113,31 @@ class StartViewController: UIViewController {
         let ref = Database.database().reference()
         ref.child("chat_room/\(my_id)&\(your_id)/\(my_id)").setValue("~~~~")
         ref.child("chat_room/\(my_id)&\(your_id)/\(your_id)").setValue("~~~~")
-        
+        //waiting_member消す
         ref.child("user/waiting_member/\(my_id)").removeValue()
         ref.child("user/waiting_member/\(your_id)").removeValue()
     }
+   
+    
+    
+    //ローディング
+    func loading() {
+        // ActivityIndicatorを作成＆中央に配置
+        self.ActivityIndicator = UIActivityIndicatorView()
+        self.ActivityIndicator.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        self.ActivityIndicator.center = self.view.center
+        // クルクルをストップした時に非表示する
+        self.ActivityIndicator.hidesWhenStopped = true
+        // 色を設定
+        self.ActivityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.white
+        self.ActivityIndicator.backgroundColor = UIColor.gray
+        self.ActivityIndicator.layer.opacity = 0.6
+        //Viewに追加
+        self.view.addSubview(self.ActivityIndicator)
+        //ロードスタート
+        self.ActivityIndicator.startAnimating()
+    }
+    
     
     
     /*

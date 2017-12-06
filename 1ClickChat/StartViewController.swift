7916bfb623edc9695a7b8220d0b08cc18c3b4538
小川秀哉 <shuya464924@gmail.com>
2017-12-06 14:45:55 +0900

    done finish message

--- 1ClickChat/StartViewController.swift
+++ 1ClickChat/StartViewController.swift
@@ -20,6 +20,7 @@ class StartViewController: VideoSplashViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         setupVideo()
      /*   ref.child("user/all_member/0").setValue("must_not_delete")
         ref.child("user/waiting_member/0").setValue("must_not_delete")
@@ -43,7 +44,7 @@ class StartViewController: VideoSplashViewController {
             fillMode = .resizeAspectFill
             alwaysRepeat = true
             restartForeground = true
-            sound = true
+            sound = false
             startTime = 0.0
             duration = 0.0
             alpha = 0.7
@@ -54,51 +55,60 @@ class StartViewController: VideoSplashViewController {
     
     
     @IBAction func StartButton(_ sender: UIButton) {
-        
+        print("66666666", self.appDelegate.my_id)
+        print("6666666666", self.appDelegate.your_id)
         button.isEnabled = false
-        
+        var my_id: AnyObject?  = nil
+        var your_id: AnyObject? = nil
+        self.appDelegate.my_id = nil
+        self.appDelegate.your_id = nil
+        print("66666666888888888888", self.appDelegate.my_id)
+        print("666666666688888888888888", self.appDelegate.your_id)
         let ref = Database.database().reference()
         
         ref.child("user").observeSingleEvent(of: .value, with: { (snapshot) in
             // Get user value
+            print("7777777777", self.appDelegate.my_id)
+            print("7777777777", self.appDelegate.your_id)
             let value = snapshot.value as! NSDictionary
             let all_member_array = value["all_member"] as AnyObject
             let my_id = all_member_array.count
             ref.child("user/all_member/\(my_id!)").setValue(my_id!)
-            print("11111111")
             
 //            let waiting_member_array = value["waiting_member"] as AnyObject
             guard let waiting_member_array = value["waiting_member"] as? AnyObject else { return }
             
-            print("22222222222")
-            print(waiting_member_array)
+           
             
-            print("waiting_member_array?.count", waiting_member_array.count)
+            
             //①自分が1人目のパターン
             if waiting_member_array.count == 1{
-                print("33333333333")
+                
                 //ローディング
                 self.loading()
+                
                 ref.child("user/waiting_member/\(my_id!)").setValue(my_id!)
                 self.appDelegate.my_id = my_id! as AnyObject
+                print("9999999999992222222222222", self.appDelegate.my_id)
+                print("99999999992222222222222", self.appDelegate.your_id)
                 //2人目のユーザーが来るまでデータ取得
                 ref.observe(.value, with: { snapshot in
-                    print("4444444444")
+                   
                     let value = snapshot.value as? NSDictionary
                     let user_array = value?["user"] as AnyObject?
                     let waiting_member_array = user_array!["waiting_member"] as AnyObject?
                     if waiting_member_array!.count == 3 {
-                        print("55555555555")
+                        
                         let all_member_array = user_array!["all_member"] as AnyObject?
                         let all_member_count = all_member_array?.count as Int?
                         self.appDelegate.your_id = all_member_count! - 1 as AnyObject
-                        print("66666666666666")
+                    
                         //ロード終了
                         self.ActivityIndicator.stopAnimating()
-                        print("7777777777777")
+                       
                         //chat_roomを作る
                         self.set_chat_room(my_id: self.appDelegate.my_id as! Int, your_id: self.appDelegate.your_id as! Int)
-                        print("88888888888")
+                       
                         
                     }
                 }) { (error) in
@@ -109,14 +119,14 @@ class StartViewController: VideoSplashViewController {
             
             //②自分が２人目のパターン
             if waiting_member_array.count == 2{
-                print("9999999999999")
+               
                 ref.child("user/waiting_member/\(my_id!)").setValue(my_id!)
                 self.appDelegate.my_id = my_id as AnyObject?
                 let all_member_array = value["all_member"] as AnyObject?
                 let all_member_count = all_member_array?.count as Int?
                 self.appDelegate.your_id = all_member_count! - 1 as AnyObject
                 self.performSegue(withIdentifier: "SegueId", sender: self)
-                print("aaaaaaaaaaaaaaa")
+               
                 
             }
             
@@ -132,23 +142,25 @@ class StartViewController: VideoSplashViewController {
     
     //chat_roomを作りwaiting_memberから削除する
     func set_chat_room(my_id: Int, your_id: Int){
-        print("bbbbbbbbbbbb")
+        print("888888888", my_id)
+        print("88888888", your_id)
         let ref = Database.database().reference()
         ref.child("chat_room/\(my_id)&\(your_id)/dammy").setValue("~~~")
         ref.child("chat_room/\(my_id)&\(your_id)/dammy").setValue("~~~")
         //waiting_member消す
         ref.child("user/waiting_member/\(my_id)").removeValue()
         ref.child("user/waiting_member/\(your_id)").removeValue()
-        print("cccccccccccccc")
+        
         self.performSegue(withIdentifier: "SegueId", sender: self)
-        print("ddddddddddddddd")
+        
     }
    
     
     
     //ローディング
     func loading() {
-        print("eeeeeeeeeee")
+        print("999999999999", self.appDelegate.my_id)
+        print("9999999999", self.appDelegate.your_id)
         // ActivityIndicatorを作成＆中央に配置
         self.ActivityIndicator = UIActivityIndicatorView()
         self.ActivityIndicator.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
@@ -161,7 +173,7 @@ class StartViewController: VideoSplashViewController {
         self.ActivityIndicator.layer.opacity = 0.6
         //Viewに追加
         self.view.addSubview(self.ActivityIndicator)
-        print("fffffffffffff")
+        
         //ロードスタート
         self.ActivityIndicator.startAnimating()
     }
--- 1ClickChat/ViewController.swift
+++ 1ClickChat/ViewController.swift
@@ -22,97 +22,140 @@ class ViewController: JSQMessagesViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("ggggggggggggggggggg")
+       
         let my_id = self.appDelegate.my_id!
         let your_id = self.appDelegate.your_id!
-        print("my_id", my_id)
-        print("your_id", your_id)
+        print("111111111",type(of: my_id))
+        print("111111111",your_id)
+        
         senderDisplayName = String(describing: my_id)
         senderId = String(describing: your_id)
-        
-        
+       
         let ref = Database.database().reference()
         ref.observe(.value, with: { snapshot in
-            print("hhhhhhhhhhhhhhhhhhhhhhhh")
-            let value = snapshot.value as! NSDictionary
-            let whole_chat_room = value["chat_room"] as AnyObject
-           
-           
-            if Int(truncating: my_id as! NSNumber) < Int(truncating: your_id as! NSNumber) {
-                print("iiiiiiiiiiiiiiiiii")
+            if (self.appDelegate.my_id != nil || self.appDelegate.your_id != nil) {
+                print("22222222226666666666666666", my_id)
+                print("22222222226666666666", your_id)
+                let my_id = self.appDelegate.my_id!
+                let your_id = self.appDelegate.your_id!
+                print("2222222222", my_id)
+                print("2222222222", your_id)
+                let value = snapshot.value as! NSDictionary
+                let whole_chat_room = value["chat_room"] as AnyObject
                 
-                //                guard let chat_room = whole_chat_room["\(my_id)&\(your_id)"] as? Dictionary<String, AnyObject> else { return }
-                guard let chat_room = whole_chat_room["\(my_id)&\(your_id)"] as? Dictionary<String, AnyObject> else { return }
-                print("chat_room", chat_room)
-                print("jjjjjjjjjjjjjjjjjj")
-                guard let number_of_chat = chat_room.count - 1 as? Int else { return }
-                guard let chat_room_content = chat_room["\(number_of_chat)"] as? Dictionary<String, AnyObject> else { return }
-                print("chat_room_content", chat_room_content)
-                print("kkkkkkkkkkkkkkkkkkkkkk")
-                for (key, value) in chat_room_content {
-                    print("lllllllllllllllllllllllll")
-                    if key == String(describing: my_id) {
-                        let text = value
-                        print("mmmmmmmmmmmmmmmmm")
-                        self.messages.append(JSQMessage(senderId: String(describing: your_id), displayName: String(describing: my_id), text: (text as! String )))
-                    }
-                    
-                    if key == String(describing: your_id) {
-                        let text = value
-                        print("nnnnnnnnnnnnnnnn")
-                        self.messages.append(JSQMessage(senderId: String(describing: my_id), displayName: String(describing: your_id), text: (text as! String )))
-                    }
+                //チャット終了処理
+                if (my_id == nil || your_id == nil) {
+                    print("44444444", my_id)
+                    print("44444444", your_id)
+                    self.end_of_chat()
                 }
                 
                 
-                self.collectionView.reloadData()
-            }
-           
-            
-            
-            if Int(truncating: my_id as! NSNumber) > Int(truncating: your_id as! NSNumber) {
-                //                guard let chat_room = whole_chat_room["\(my_id)&\(your_id)"] as? Dictionary<String, AnyObject> else { return }
-                guard let chat_room = whole_chat_room["\(your_id)&\(my_id)"] as? Dictionary<String, AnyObject> else { return }
-                print("oooooooooooooooooooo")
-               
-                guard let number_of_chat = chat_room.count - 1 as? Int else { return }
-                guard let chat_room_content = chat_room["\(number_of_chat)"] as? Dictionary<String, AnyObject> else { return }
-               
-                print("qqqqqqqqqqqqqq")
-                for (key, value) in chat_room_content {
-                    print("rrrrrrrrrrrrrrr")
-                    if key == String(describing: my_id) {
-                        let text = value
-                        print("ssssssssssssss")
-                        self.messages.append(JSQMessage(senderId: String(describing: your_id), displayName: String(describing: my_id), text: (text as! String)))
+                if Int(truncating: my_id as! NSNumber) < Int(truncating: your_id as! NSNumber) {
+                    
+                    //                guard let chat_room = whole_chat_room["\(my_id)&\(your_id)"] as? Dictionary<String, AnyObject> else { return }
+                    guard let chat_room = whole_chat_room["\(my_id)&\(your_id)"] as? Dictionary<String, AnyObject> else { return }
+                    
+                    guard let number_of_chat = chat_room.count - 1 as? Int else { return }
+                    guard let chat_room_content = chat_room["\(number_of_chat)"] as? Dictionary<String, AnyObject> else { return }
+                    for (key, value) in chat_room_content {
+                        if key == String(describing: my_id) {
+                            let text = value
+                            
+                            self.messages.append(JSQMessage(senderId: String(describing: your_id), displayName: String(describing: my_id), text: (text as! String )))
+                        }
+                        
+                        if key == String(describing: your_id) {
+                            let text = value
+                            if text as! String == "退出しました。" {
+                                self.end_of_chat()
+                            }
+                            self.messages.append(JSQMessage(senderId: String(describing: my_id), displayName: String(describing: your_id), text: (text as! String )))
+                        }
                     }
                     
-                    if key == String(describing: your_id) {
-                        let text = value
-                        print("tttttttttttttt")
-                        self.messages.append(JSQMessage(senderId: String(describing: my_id), displayName: String(describing: your_id), text: (text as! String)))
-                    }
+                    
+                    self.collectionView.reloadData()
                 }
+               
                 
                 
-                self.collectionView.reloadData()
-            }
-         
-        })
+                if Int(truncating: my_id as! NSNumber) > Int(truncating: your_id as! NSNumber) {
+                    
+                    
+                    //                guard let chat_room = whole_chat_room["\(my_id)&\(your_id)"] as? Dictionary<String, AnyObject> else { return }
+                    guard let chat_room = whole_chat_room["\(your_id)&\(my_id)"] as? Dictionary<String, AnyObject> else { return }
+                    
+                   
+                    guard let number_of_chat = chat_room.count - 1 as? Int else { return }
+                    guard let chat_room_content = chat_room["\(number_of_chat)"] as? Dictionary<String, AnyObject> else { return }
+                   
+                   
+                    for (key, value) in chat_room_content {
+                        if key == String(describing: my_id) {
+                            let text = value
+                            
+                            self.messages.append(JSQMessage(senderId: String(describing: your_id), displayName: String(describing: my_id), text: (text as! String)))
+                        }
+                        
+                        if key == String(describing: your_id) {
+                            let text = value
+                            if text as! String == "退出しました。" {
+                                self.end_of_chat()
+                            }
+                            self.messages.append(JSQMessage(senderId: String(describing: my_id), displayName: String(describing: your_id), text: (text as! String)))
+                        }
+                    }
+                    self.collectionView.reloadData()
+                }
+                }
+            })
+            
+        
     }
     
 
     //添付ボタン＝ログアウトボタン
     override func didPressAccessoryButton(_ sender: UIButton!) {
+        print("aaaaaaaaaaaaaaaaaaa")
         let alert: UIAlertController = UIAlertController(title: "", message: "チャットを終了してもよろしいですか？", preferredStyle:  UIAlertControllerStyle.alert)
-        
+        let ref = Database.database().reference()
+        let my_id = self.appDelegate.my_id!
+        let your_id = self.appDelegate.your_id!
         let defaultAction: UIAlertAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler:{
             // ボタンが押された時の処理を書く（クロージャ実装）
             (action: UIAlertAction!) -> Void in
-            var my_id: AnyObject?
-            var your_id: AnyObject?
-            print("my_id", my_id)
+            if Int(truncating: my_id as! NSNumber) < Int(truncating: your_id as! NSNumber) {
+                ref.child("chat_room").observeSingleEvent(of: .value, with: { (snapshot) in
+                    let value = snapshot.value as! NSDictionary
+                    let chat_room = value["\(my_id)&\(your_id)"] as AnyObject
+                    let number_of_chat = chat_room.count
+                    let text = "退出しました。"
+                    ref.child("chat_room/\(my_id)&\(your_id)/\(number_of_chat!)").updateChildValues(["\(my_id)": text])
+                }) { (error) in
+                    print(error.localizedDescription)
+                }
+            }
+            if Int(truncating: my_id as! NSNumber) > Int(truncating: your_id as! NSNumber) {
+                ref.child("chat_room").observeSingleEvent(of: .value, with: { (snapshot) in
+                    let value = snapshot.value as! NSDictionary
+                    let chat_room = value["\(your_id)&\(my_id)"] as AnyObject
+                    let number_of_chat = chat_room.count
+                    let text = "退出しました。"
+                    ref.child("chat_room/\(your_id)&\(my_id)/\(number_of_chat!)").updateChildValues(["\(my_id)": text])
+                }) { (error) in
+                    print(error.localizedDescription)
+                }
+            }
+            
+            let my_id: AnyObject? = nil
+            let your_id: AnyObject? = nil
+            self.appDelegate.my_id = nil
+            self.appDelegate.your_id = nil
+            print("33333333", my_id)
+            print("33333333", your_id)
             self.performSegue(withIdentifier: "SegueId2", sender: self)
+            print("bbbbbbbbbbbbbbbbbbbb")
         })
         let cancelAction: UIAlertAction = UIAlertAction(title: "キャンセル", style: UIAlertActionStyle.cancel, handler:{
             // ボタンが押された時の処理を書く（クロージャ実装）
@@ -123,8 +166,26 @@ class ViewController: JSQMessagesViewController {
         present(alert, animated: true, completion: nil)
     }
     
+    func end_of_chat () {
+        let alert: UIAlertController = UIAlertController(title: "チャットが終了しました", message: "ホームに戻ります。", preferredStyle:  UIAlertControllerStyle.alert)
+        let defaultAction: UIAlertAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler:{
+            // ボタンが押された時の処理を書く（クロージャ実装）
+            (action: UIAlertAction!) -> Void in
+            self.performSegue(withIdentifier: "SegueId2", sender: self)
+            print("5555555555", self.appDelegate.my_id)
+            print("55555555", self.appDelegate.your_id)
+        })
+        alert.addAction(defaultAction)
+        present(alert, animated: true, completion: nil)
+    }
     
     
+    override func viewDidDisappear(_ animated: Bool) {
+        var my_id: AnyObject?  = nil
+        var your_id: AnyObject? = nil
+        self.appDelegate.my_id = nil
+        self.appDelegate.your_id = nil
+    }
  
     
     override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageDataForItemAt indexPath: IndexPath!) -> JSQMessageData! {
@@ -187,8 +248,6 @@ class ViewController: JSQMessagesViewController {
         let my_id = self.appDelegate.my_id!
         let your_id = self.appDelegate.your_id!
        
-        
-        
         if Int(truncating: my_id as! NSNumber) < Int(truncating: your_id as! NSNumber) {
             ref.child("chat_room").observeSingleEvent(of: .value, with: { (snapshot) in
                 let value = snapshot.value as! NSDictionary
